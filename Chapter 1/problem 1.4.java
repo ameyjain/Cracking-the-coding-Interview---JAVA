c8d0c5194c3934fd9e1d4c007d0231aa13d2574c
Amey Jain <ajain@apppartner.com>
2018-01-15 13:26:15 -0500

    Solution for 1.4

---
+++ Chapter 1/problem 1.4.java
@@ -0,0 +1,23 @@
+public class MyClass {
+    public static void main(String args[]) {
+        System.out.println("is permutable palindrome = " + solution("aab"));
+    }
+
+    public static boolean solution(String s) {
+
+        int checker = 0;
+        String string = s.trim().replaceAll(" ", "");
+
+        if (string.isEmpty()) return;
+
+        for (Character c : s.toCharArray()) {
+
+            if ((checker & (1 << c)) == 0) {
+                checker = checker ^ (1 << c);
+            }
+            checker |= (1 << c);
+        }
+
+        return checker == 0 || checker % 2 == 0;
+    }
+}
